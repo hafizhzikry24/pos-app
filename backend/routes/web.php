f29d6bb07ea955aaf9e727520924df37731571af
hafizhzikry24 <hafizhcool24@gmail.com>
2025-12-11 23:09:32 +0700

    Initial monorepo commit

---
+++ backend/routes/web.php
@@ -0,0 +1,10 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+Route::get('/', function () {
+    return response()->json([
+        'error' => false,
+        'message' => 'Successfully running the API Pos App'
+    ]);
+});
